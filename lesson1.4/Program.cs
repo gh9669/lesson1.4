1c122188144751a2d33e4f643b336bbc5accaad8
Flvby <pupok.v.66@gmail.com>
2022-07-22 15:05:35 +0300

    Добавьте файлы проекта.

---
+++ lesson1.4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lesson1._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        private static void pers()
+        {
+            Dictionary<string, string> characterName = new Dictionary<string, string>(3);
+            characterName.Add("knight" , "sword");
+            //["archer"] = new Archer();
+            //characterName["mage"] = new Mage();
+
+            foreach (KeyValuePair<string, string> keyValue in characterName)
+            {
+                Console.WriteLine(keyValue.Key + "  " + keyValue.Value);
+            }
+        }
+
+
+        
+    }
+}
